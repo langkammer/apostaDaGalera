f4bce6a1aef1f9f2926ab6dbe12f62e3218e651a
Robson <robsonlangkammer@hotmail.com>
2019-06-14 17:53:43 -0300

    melhorias

--- src/app/login/modal/login-modal.component.ts
+++ src/app/login/modal/login-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -9,9 +9,15 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class LoginModalComponent {
   constructor(private modalService: NgbModal, public activeModal: NgbActiveModal) {}
 
+  @Input() usuario: any = {};
+
   open() {
-    this.modalService.open(LoginModalComponent, {
-      size: 'lg'
-    });
+    this.modalService.open(LoginModalComponent, 
+      {
+        ariaLabelledBy: 'modal-basic-title', 
+        size: 'lg', 
+         windowClass: 'custom-class'
+    }
+    );
   }
 }
